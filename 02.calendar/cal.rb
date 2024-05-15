f5283c1d2fa6aec209a02dd7440393104a70b73a
TsukagoshiShunsuke <shunsuke.tsukagoshi@luxiar.com>
2024-05-15 17:42:37 +0900

    色々と書き直しました

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,61 +1,41 @@
 #!/usr/bin/env ruby
 
+# frozen_string_literal: true
+
 require 'date'
 require 'optparse'
-
-class Calendar
-  attr_reader :year, :month, :day_of_week, :days
-
-  def initialize(year: nil, month: nil)
-    @day = year.nil? || month.nil? ? Date.today : Date.new(year, month, 1)
-    @year = @day.year
-    @month = @day.month
-    @day_of_week = ['日', '月', '火', '水', '木', '金', '土']
-    @days = []
-
-    days << Array.new(7) {|i| i = 0}
-    day_index = Date.new(@year, @month, 1).cwday % 7
-    days[-1][day_index] = 1
-    day_index += 1
-    max_day = @day.next_month.prev_day.day
-
-    (2..max_day).each do |day|
-      if day_index == 7
-        @days << Array.new(7) {|i| i = 0}
-        day_index = 0
-      end
-
-      @days[-1][day_index] = day
-      day_index += 1
-    end
-  end
-
-  def display_calendar
-    puts "     #{month.to_s.rjust(2)}月  #{year}"
-    puts day_of_week.join(' ')
-
-    days.each do |week|
-      week.each do |day|
-        day='' if day == 0
-        print day.to_s.rjust(2) + ' '
-      end
-      puts
-    end
-  end
-end
-
-opt = OptionParser.new
 
 year = Date.today.year
 month = Date.today.month
 
-opt.on('-y VAL'){|v| year = v.to_i}
-opt.on('-m VAL'){|v| month = v.to_i}
+opt = OptionParser.new
+opt.on('-y VAL') { |v| year = v.to_i }
+opt.on('-m VAL') { |v| month = v.to_i }
 opt.parse!(ARGV)
 
-if year < 1 || month < 1 || month > 12
-  puts 'invalid option'
-  exit
+selected_date = Date.new(year, month, 1)
+dates = [Array.new(7, 0)]
+date_index = selected_date.cwday % 7
+dates[0][date_index] = 1
+date_index += 1
+
+(2..selected_date.next_month.prev_day.day).each do |date|
+  if date_index == 7
+    dates << Array.new(7, 0)
+    date_index = 0
+  end
+
+  dates[-1][date_index] = date
+  date_index += 1
 end
 
-Calendar.new(year: year, month: month).display_calendar
+puts "     #{month.to_s.rjust(2)}月 #{year}"
+puts '日 月 火 水 木 金 土'
+
+dates.each do |week|
+  week.each do |date|
+    date = '' if date.zero?
+    print "#{date.to_s.rjust(2)} "
+  end
+  puts
+end
