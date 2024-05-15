7b594c2c9943d1b205b5dcf31cbe666a180da4cf
TsukagoshiShunsuke <shunsuke.tsukagoshi@luxiar.com>
2024-05-15 15:25:52 +0900

    add sort of files rename file_array files use Dir.glob

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,17 +5,32 @@
 COLUMNS = 3
 COLUMN_WIDTH = 30
 
-def display_files(file_array)
-  file_array.each_slice(COLUMNS) do |files|
-    puts files.map { |file| file + ' ' * calc_margin_length(file) }.join
+def display_files(files)
+  files = sort_for_display(files)
+  files.each do |file_set|
+    puts file_set.map { |file| file + ' ' * calc_margin_length(file) }.join
   end
 end
 
 def calc_margin_length(file_name)
-  COLUMN_WIDTH - file_name.gsub(/[^ -~｡-ﾟ\s]/, 'aa').length
+  COLUMN_WIDTH - (file_name.length + file_name.scan(/[^ -~｡-ﾟ\s]/).length)
 end
 
-all_files = Dir.entries('.')
-normal_files = all_files.reject { |file| file[0] == '.' }
+def sort_for_display(files)
+  (rows = files.length % 3).zero? ? files.length / 3 : files.length / 3 + 1
+  sorted_files = Array.new(rows) { [] }
+  files = files.sort
 
-display_files(normal_files)
+  2.times do
+    rows.times do |row|
+      sorted_files[row] << files.shift
+    end
+  end
+  files.each_with_index do |file, index|
+    sorted_files[index] << file
+  end
+
+  sorted_files
+end
+
+display_files(Dir.glob('*'))
