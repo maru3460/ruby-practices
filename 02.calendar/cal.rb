4a707483a49ce15863a50296e7a8b9155a9703a9
TsukagoshiShunsuke <shunsuke.tsukagoshi@luxiar.com>
2024-05-17 10:45:38 +0900

    fill list nil instead of 0

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -13,20 +13,14 @@ opt.on('-y VAL') { |v| year = v.to_i }
 opt.on('-m VAL') { |v| month = v.to_i }
 opt.parse!(ARGV)
 
-selected_date = Date.new(year, month, 1)
-dates = [Array.new(7, 0)]
-date_index = selected_date.cwday % 7
-dates[0][date_index] = 1
-date_index += 1
-
-(2..selected_date.next_month.prev_day.day).each do |date|
-  if date_index == 7
-    dates << Array.new(7, 0)
-    date_index = 0
+first_date = Date.new(year, month, 1)
+last_date = Date.new(year, month, -1)
+dates = []
+(first_date..last_date).each do |date|
+  if date.sunday? || date.day == 1
+    dates << []
   end
-
-  dates[-1][date_index] = date
-  date_index += 1
+  dates[-1][date.wday] = date.day
 end
 
 puts "     #{month.to_s.rjust(2)}月 #{year}"
@@ -34,7 +28,6 @@ puts '日 月 火 水 木 金 土'
 
 dates.each do |week|
   week.each do |date|
-    date = '' if date.zero?
     print "#{date.to_s.rjust(2)} "
   end
   puts
